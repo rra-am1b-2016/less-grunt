b9dae6252e711c3087a249d3c448eefbadafd695
Arjan de Ruijter <rra.am1b.2016@gmail.com>
2017-03-09 12:55:56 +0100

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+  require('jit-grunt')(grunt);
+
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          compress: false,
+          yuicompress: false,
+          optimization: 2
+        },
+        files: {
+          "./css/test.css": "./less/rra.less" // destination file and source file
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['./less/**/*.less'], // which files to watch
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['less', 'watch']);
+};
+
