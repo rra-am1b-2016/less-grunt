d0d3a9cc9bbdd6ae1ec57b98e6b47b4a864f44a8
Arjan de Ruijter <rra.am1b.2016@gmail.com>
2017-03-10 14:00:03 +0100

    guarded-mixin

---
+++ guarded_mixin.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <title>Guarded mixin</title>
+   <link rel="stylesheet" href="./css/test.css">
+</head>
+<body>
+   <div id='div-id'>
+      div#guarded-mixin: Deze div heeft voorwaardelijke css-opmaak.
+   </div>
+</body>
+</html>
---
+++ mixin.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <title>Less en Grunt</title>
+   <link rel="stylesheet" href="./css/test.css">
+</head>
+<body>
+   <div id="content-1">
+      Dit is divje 1
+   </div>
+
+   <div id="content-2">
+      Dit is divje 2
+   </div>
+
+   <div id="content-3">
+      Dit is divje 3
+   </div>
+
+   <div id="content-4">
+      Dit is divje 4
+   </div>
+
+   <div id="content-5">
+      Dit is divje 5
+   </div>   
+</body>
+</html>
